65d6f5d5438ba24a4a612bb38dd4b75960b811de
Colin <colin.luo@magicwindow.cn>
2016-02-17 11:10:51 +0800

    Fixed "Promise is not defined" on Android 4.x

--- src/marketing.js
+++ src/marketing.js
@@ -62,26 +62,26 @@ export default class Marketing {
    */
   loadPromise () {
 
-    return new Promise(function(resolve, reject) {
-
-      // marketing/v2?ak=XEJ7F76J61LHEWRI3Q9A6UN9BM4CRT3X&os=0&sv=2.3&d=864387021280405&sr=720x1280&av=2.3&fp=155439573
-      var macketingUrl = '{SERVER}/marketing/v2?ak={AK}&os={OS}&sv={SV}&d={D}&sr={SR}&av={AV}&fp={fp}';
-      var params = this.getParams();
-      var url = this.applyParams(macketingUrl, params);
-      var ajax = new Ajax();
-
-      ajax.http({
-        url: url,
-        type: 'get',
-        dataType: 'jsonp',
-        success: function(data) {
-          resolve(data);
-        },
-        error: function() {
-          reject('error');
-        }
-      });
-
-    }.bind(this));
+    //return new Promise((resolve, reject) => {
+    //
+    //  // marketing/v2?ak=XEJ7F76J61LHEWRI3Q9A6UN9BM4CRT3X&os=0&sv=2.3&d=864387021280405&sr=720x1280&av=2.3&fp=155439573
+    //  var macketingUrl = '{SERVER}/marketing/v2?ak={AK}&os={OS}&sv={SV}&d={D}&sr={SR}&av={AV}&fp={fp}';
+    //  var params = this.getParams();
+    //  var url = this.applyParams(macketingUrl, params);
+    //  var ajax = new Ajax();
+    //
+    //  ajax.http({
+    //    url: url,
+    //    type: 'get',
+    //    dataType: 'jsonp',
+    //    success: function(data) {
+    //      resolve(data);
+    //    },
+    //    error: function() {
+    //      reject('error');
+    //    }
+    //  });
+    //
+    //});
   }
 }
