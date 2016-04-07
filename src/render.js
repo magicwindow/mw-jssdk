302f84746a735c063960047979a48bd344bdd174
Colin <colin.luo@magicwindow.cn>
2016-04-07 15:31:54 +0800

    c

--- src/render.js
+++ src/render.js
@@ -192,7 +192,7 @@ export default class Render {
     dialog.id = PREFIX_DIALOG_ID + '-' + mwBlock.id;
     dialog.classList.add(PREFIX_DIALOG_ID);
     dialog.innerHTML = '<div class="' + PREFIX_DIALOG_ID + '-toolbar"><a class="closeMWBlock" href="javascript:void(0);"> </a></div>' +
-      '<iframe src="about:blank" frameborder="0"></iframe>';
+      '<div class="'+ PREFIX_DIALOG_ID +'-main"><iframe src="about:blank" frameborder="0"></iframe></div>';
     btnClose = dialog.getElementsByTagName('a')[0];
     btnClose.addEventListener('click', () => {
       dialog.parentNode.removeChild(dialog);
